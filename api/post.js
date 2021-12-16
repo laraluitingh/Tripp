780957482d74d6fcf0a33a46a5c18ac5336f7737
laraluitingh <61213237+laraluitingh@users.noreply.github.com>
2021-12-16 21:13:27 +1100

    changes

--- api/post.js
+++ api/post.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Post = require("./../models/Post");
+const Post = require("./../models/post");
 const user= require("./../models/User");
 
 
